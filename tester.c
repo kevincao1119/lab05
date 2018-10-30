812ed11de9107b7c54c0c55c1c4d8692abd54174
kevincao1119 <43764834+kevincao1119@users.noreply.github.com>
2018-10-30 10:26:58 -0400

    Update tester.c

--- tester.c
+++ tester.c
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
   
 
     printf("Trying to connect to server\n");
-    ifttt("https://maker.ifttt.com/trigger/alarm_triggered/with/key/dcZuaJf3oHEAyHhu-55bJE", "my1", "my 2", "my 33333");
+    ifttt("https://maker.ifttt.com/trigger/alarm_triggered/with/key/alarm_triggered/with/key/dcZuaJf3oHEAyHhu-55bJE", "my1", "my 2", "my 33333");
     return 0;
 }
